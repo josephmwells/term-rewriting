4b99ccb6a5f894c5a59ec76a0e0c3cfbc3a5b063
josephmwells <joester3@gmail.com>
2020-05-16 15:16:22 -0700

    implemented copy constructor and assignment operator

--- term.hpp
+++ term.hpp
@@ -36,6 +36,10 @@ using rule = std::pair<term_ptr<T>, term_ptr<T>>;
 //
 /////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////
+// TERM
+/////////////////////////////////////////////////////////////////
+
 template<typename T>
 class term { 
   public:
@@ -50,59 +54,120 @@ class term {
   private:
 };
 
+/////////////////////////////////////////////////////////////////
+// VARIABLE
+/////////////////////////////////////////////////////////////////
+
 template<typename T>
 class variable : public term<T> { 
   public:
     variable<T>() : term<T>() {};
+    variable<T>(const variable<T>& copy) : term<T>(), name_{copy.name_} {};
     variable<T>(std::string name) : term<T>(), name_{name} {};
 
-    void print() const
-    {
-      std::cout << name_;
-    }
-    
+    variable<T>& operator=(const variable<T>& other);
+
+    void print() const override;
+
   private:
     std::string name_;
 };
 
 template<typename T>
+variable<T>& variable<T>::operator=(const variable<T>& other)
+{
+    name_ = other.name_;
+    return *this;
+}
+
+template<typename T>
+void variable<T>::print() const
+{
+    std::cout << name_;
+}
+
+/////////////////////////////////////////////////////////////////
+// LITERAL
+/////////////////////////////////////////////////////////////////
+
+template<typename T>
 class literal : public term<T> { 
   public:
     literal<T>() : term<T>() {};
+    literal<T>(const literal<T>& copy) : term<T>(), value_{copy.value_} {};
     literal<T>(T value) : term<T>(), value_{value} {};
 
-    void print() const
-    {
-      std::cout << value_;
-    }
+    literal<T>& operator=(const literal<T>& other);
+
+    void print() const override;
 
   private:
     T value_;
 };
 
 template<typename T>
+literal<T>& literal<T>::operator=(const literal<T>& other)
+{
+    value_ = other.value_;
+    return *this;
+}
+
+template<typename T>
+void literal<T>::print() const
+{
+    std::cout << value_;
+}
+
+template<>
+void literal<bool>::print() const
+{
+    if(value_) std::cout << "true";
+    if(!value_) std::cout << "false";
+}
+
+/////////////////////////////////////////////////////////////////
+// FUNCTION
+/////////////////////////////////////////////////////////////////
+
+template<typename T>
 class function : public term<T> { 
   public:
     function<T>() : term<T>() {};
-    function<T>(std::string name, int arity, std::vector<term_ptr<T>> children) 
-      : term<T>(), name_{name}, arity_{arity}, children_{children} {}; 
+    function<T>(const function<T>& copy)
+            : term<T>(), name_{copy.name_}, arity_{copy.arity_}, children_{copy.children_} {};
+    function<T>(std::string name, int arity, std::vector<term_ptr<T>> children)
+            : term<T>(), name_{name}, arity_{arity}, children_{children} {};
 
-    void print() const
-    {
-      std::cout << name_;
-      std::cout << "(";
-      for(const term_ptr<T>& t : children_) {
-        t->print();
-        if(t != children_.back()) std::cout << ", ";
-      }
-      std::cout << ")";
-    }
-    
+    function<T>& operator=(const function<T>& other);
+
+    void print() const override;
+
   private:
     std::string name_;
     int arity_;
     std::vector<term_ptr<T>> children_;
 };
+
+template<typename T>
+function<T>& function<T>::operator=(const function<T>& other)
+{
+    name_ = other.name_;
+    arity_ = other.arity_;
+    children_ = other.children_;
+    return *this;
+}
+
+template<typename T>
+void function<T>::print() const
+{
+    std::cout << name_;
+    std::cout << "(";
+    for(const term_ptr<T>& t : children_) {
+        t->print();
+        if (t != children_.back()) std::cout << ", ";
+    }
+    std::cout << ")";
+}
 
 
 /////////////////////////////////////////////////////////////////
