a43e221ba7f01467c93976ed63010b5463dba544
josephmwells <joester3@gmail.com>
2020-05-17 15:07:16 -0700

    implemented forward iterator and ++ operator

--- term.hpp
+++ term.hpp
@@ -41,17 +41,29 @@ using rule = std::pair<term_ptr<T>, term_ptr<T>>;
 /////////////////////////////////////////////////////////////////
 
 template<typename T>
-class term { 
-  public:
-    
+class term {
+public:
+    typedef term<T> value_type;
+    typedef term_ptr<T> pointer;
+    typedef term<T>& reference;
+    typedef size_t size_type;
+    typedef ptrdiff_t difference_type;
+    typedef std::forward_iterator_tag iterator_category;
+    typedef term_iterator<T> iterator;
+    typedef term_iterator<const T> const_iterator;
+
     term<T>() {};
 
-    term_iterator<T> begin() { return term_iterator<T>(); }
-    term_iterator<T> end() { return term_iterator<T>(); }
+    iterator begin() { return iterator(*this, true); }
+
+    iterator end() { return iterator(*this, false); }
+
+    virtual std::vector<term_ptr<T>> children() { return std::vector<term_ptr<T>>(); }
+
 
     virtual void print() const = 0;
 
-  private:
+private:
 };
 
 /////////////////////////////////////////////////////////////////
@@ -59,30 +71,30 @@ class term {
 /////////////////////////////////////////////////////////////////
 
 template<typename T>
-class variable : public term<T> { 
-  public:
+class variable : public term<T> {
+public:
     variable<T>() : term<T>() {};
-    variable<T>(const variable<T>& copy) : term<T>(), name_{copy.name_} {};
+
+    variable<T>(const variable<T> &copy) : term<T>(), name_{copy.name_} {};
+
     variable<T>(std::string name) : term<T>(), name_{name} {};
 
-    variable<T>& operator=(const variable<T>& other);
+    variable<T>& operator=(const variable<T> &other);
 
     void print() const override;
 
-  private:
+private:
     std::string name_;
 };
 
 template<typename T>
-variable<T>& variable<T>::operator=(const variable<T>& other)
-{
+variable<T>& variable<T>::operator=(const variable<T> &other) {
     name_ = other.name_;
     return *this;
 }
 
 template<typename T>
-void variable<T>::print() const
-{
+void variable<T>::print() const {
     std::cout << name_;
 }
 
@@ -91,38 +103,37 @@ void variable<T>::print() const
 /////////////////////////////////////////////////////////////////
 
 template<typename T>
-class literal : public term<T> { 
-  public:
+class literal : public term<T> {
+public:
     literal<T>() : term<T>() {};
-    literal<T>(const literal<T>& copy) : term<T>(), value_{copy.value_} {};
+
+    literal<T>(const literal<T> &copy) : term<T>(), value_{copy.value_} {};
+
     literal<T>(T value) : term<T>(), value_{value} {};
 
-    literal<T>& operator=(const literal<T>& other);
+    literal<T>& operator=(const literal<T> &other);
 
     void print() const override;
 
-  private:
+private:
     T value_;
 };
 
 template<typename T>
-literal<T>& literal<T>::operator=(const literal<T>& other)
-{
+literal<T>& literal<T>::operator=(const literal<T> &other) {
     value_ = other.value_;
     return *this;
 }
 
 template<typename T>
-void literal<T>::print() const
-{
+void literal<T>::print() const {
     std::cout << value_;
 }
 
 template<>
-void literal<bool>::print() const
-{
-    if(value_) std::cout << "true";
-    if(!value_) std::cout << "false";
+void literal<bool>::print() const {
+    if (value_) std::cout << "true";
+    if (!value_) std::cout << "false";
 }
 
 /////////////////////////////////////////////////////////////////
@@ -130,27 +141,30 @@ void literal<bool>::print() const
 /////////////////////////////////////////////////////////////////
 
 template<typename T>
-class function : public term<T> { 
-  public:
+class function : public term<T> {
+public:
     function<T>() : term<T>() {};
-    function<T>(const function<T>& copy)
+
+    function<T>(const function<T> &copy)
             : term<T>(), name_{copy.name_}, arity_{copy.arity_}, children_{copy.children_} {};
+
     function<T>(std::string name, int arity, std::vector<term_ptr<T>> children)
             : term<T>(), name_{name}, arity_{arity}, children_{children} {};
 
-    function<T>& operator=(const function<T>& other);
+    function<T>& operator=(const function<T> &other);
+
+    std::vector<term_ptr<T>> children() override { return children_; };
 
     void print() const override;
 
-  private:
+private:
     std::string name_;
     int arity_;
     std::vector<term_ptr<T>> children_;
 };
 
 template<typename T>
-function<T>& function<T>::operator=(const function<T>& other)
-{
+function<T>& function<T>::operator=(const function<T> &other) {
     name_ = other.name_;
     arity_ = other.arity_;
     children_ = other.children_;
@@ -158,17 +172,15 @@ function<T>& function<T>::operator=(const function<T>& other)
 }
 
 template<typename T>
-void function<T>::print() const
-{
+void function<T>::print() const {
     std::cout << name_;
     std::cout << "(";
-    for(const term_ptr<T>& t : children_) {
+    for (const term_ptr<T> &t : children_) {
         t->print();
         if (t != children_.back()) std::cout << ", ";
     }
     std::cout << ")";
 }
-
 
 /////////////////////////////////////////////////////////////////
 //
@@ -177,8 +189,7 @@ void function<T>::print() const
 /////////////////////////////////////////////////////////////////
 
 template<typename T>
-bool unify(const term<T>& t1, const term<T>& t2, Sub<T>& sigma)
-{
+bool unify(const term<T> &t1, const term<T> &t2, Sub<T> &sigma) {
 }
 
 /////////////////////////////////////////////////////////////////
@@ -188,8 +199,7 @@ bool unify(const term<T>& t1, const term<T>& t2, Sub<T>& sigma)
 /////////////////////////////////////////////////////////////////
 
 template<typename T>
-term_ptr<T> reduce(term_ptr<T> t, const std::vector<rule<T>>& rules)
-{
+term_ptr<T> reduce(term_ptr<T> t, const std::vector<rule<T>> &rules) {
 }
 
 /////////////////////////////////////////////////////////////////
@@ -199,8 +209,9 @@ term_ptr<T> reduce(term_ptr<T> t, const std::vector<rule<T>>& rules)
 /////////////////////////////////////////////////////////////////
 
 template<typename T>
-term_ptr<T> rewrite(term_ptr<T> t, term<T>& rhs, std::vector<int> path, const Sub<T>& sigma)
-{
+term_ptr<T> rewrite(term_ptr<T> t, term<T>& rhs, std::vector<int> path, const Sub<T>& sigma) {
+
+    return t;
 }
 
 /////////////////////////////////////////////////////////////////
@@ -210,10 +221,9 @@ term_ptr<T> rewrite(term_ptr<T> t, term<T>& rhs, std::vector<int> path, const Su
 /////////////////////////////////////////////////////////////////
 
 template<typename T>
-std::ostream& operator<<(std::ostream& out, const term<T>& t)
-{
-  t.print();
-  return out;
+std::ostream &operator<<(std::ostream& out, const term<T>& t) {
+    t.print();
+    return out;
 }
 
 #endif // TERM_HPP
--- term_iterator.hpp
+++ term_iterator.hpp
@@ -1,36 +1,72 @@
-#ifndef TERM_ITER_HPP 
+#ifndef TERM_ITER_HPP
 #define TERM_ITER_HPP
 
 #include<memory>
 #include<vector>
 #include<stack>
 #include<string>
-#include <algorithm>
+#include<algorithm>
 
 template<typename T>
 using term_ptr = std::shared_ptr<term<T>>;
 
 template<typename T>
 class term_iterator {
-  public:
-    term_iterator<T>() {};
-  
-    term<T>& operator*() {return *root_;}
+public:
+    typedef term<T> value_type;
+    typedef term<T> &reference;
+    typedef term_ptr<T> pointer;
+    typedef size_t size_type;
+    typedef ptrdiff_t difference_type;
+    typedef std::forward_iterator_tag iterator_category;
+
+    term_iterator<T>() = delete;
+
+    term_iterator<T>(term<T> &t, bool begin);
+
+    term<T>& operator*() { return *path_.top(); }
+
     term_iterator<T>& operator++();
-    friend bool operator!=(term_iterator<T>& lhs, term_iterator<T>& rhs)
-    {
-      return false;
+
+    friend bool operator!=(term_iterator<T> &lhs, term_iterator<T> &rhs) {
+        return lhs.path_ != rhs.path_;
     }
 
-  private:
-    term_ptr<T> root_;
-    std::stack<term_ptr<T>> path_;
+private:
+    term<T>* root_;
+    std::stack<term<T>*> path_;
 };
 
 template<typename T>
-term_iterator<T>& term_iterator<T>::operator++()
-{
-  return *this;
+term_iterator<T>::term_iterator(term<T>& t, bool begin) {
+    root_ = &t;
+    if (begin) {
+        term<T>* tmp = &t;
+        for(; tmp->children().size() > 0; tmp = tmp->children()[0].get()) {
+            path_.push(tmp);
+        }
+        path_.push(tmp);
+    }
+}
+
+template<typename T>
+term_iterator<T>& term_iterator<T>::operator++() {
+    if(!path_.empty()) {
+        if(path_.top()->children().size() >= 2) {
+            path_.push(path_.top()->children()[1].get());
+            while(path_.top()->children().size() >= 1) {
+                path_.push(path_.top()->children()[0].get());
+            }
+        } else {
+            term<T>* child = path_.top();
+            path_.pop();
+            while(!path_.empty() && path_.top()->children().size() >= 2 && path_.top()->children()[1].get() == child) {
+                child = path_.top();
+                path_.pop();
+            }
+        }
+    }
+    return *this;
 }
 
 #endif // TERM_ITER_HPP
